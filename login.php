ae7d164c54db32f682fa1ccb811a2ed8ec84581e
Anthony <anthony-haslett@outlook.com>
2018-04-18 13:03:15 +0100

    Continued MVC efforts

---
+++ login.php
@@ -0,0 +1,9 @@
+<?php require_once('Views/login.phtml');
+
+//if (isset('loginSubmit')){
+// call login method in model to login user in
+//}
+
+//if (isset('signUpSubmit')){
+// call sign up  method in model to sign user up
+//}
